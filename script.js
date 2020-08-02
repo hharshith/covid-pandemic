478a14307828e8dc37f279b914a4982bf427470a
Harshith <64634096+hharshith@users.noreply.github.com>
2020-08-02 15:10:05 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function sayHello (event) {
-	var name = 
-	 document.getElementById("name").value;
-	 var message ="<h2>Hello " + name + "!</h2>";
-
-	 document.getElementById("content").innerHTML =message;
-
-
-	 }
