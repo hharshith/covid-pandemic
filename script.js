d9449bc7c79f58a22a5828ff278deb8b6237e47f
hharshith <gummadavelliharshith@gmail.com>
2020-08-02 15:09:17 +0530

    added files

---
+++ script.js
@@ -0,0 +1,9 @@
+function sayHello (event) {
+	var name = 
+	 document.getElementById("name").value;
+	 var message ="<h2>Hello " + name + "!</h2>";
+
+	 document.getElementById("content").innerHTML =message;
+
+
+	 }
